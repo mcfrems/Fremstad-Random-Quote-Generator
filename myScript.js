027b36954cb5d4587b0d8cb414b612fcf79cd12c
mcfrems <matthew.fremstad@gmail.com>
2022-12-28 15:48:08 -0600

    initial JS

--- myScript.js
+++ myScript.js
@@ -1,3 +1,49 @@
-h1 {
-    font-family: Arial, Helvetica, sans-serif;
-}
+console.log(1+Math.floor(Math.random()*100))
+
+/*alert("hi");
+
+const apiKey = "acc785dd4ef0fe813c807aa2818b9994";
+
+const testurl = "https://api.openweathermap.org/data/2.5/weather?zip=68803&units=imperial&appid=acc785dd4ef0fe813c807aa2818b9994";
+
+//test the api
+fetch(testurl)
+.then(res =>res.json())
+.then(data => {
+    console.log(data);
+    console.log(data.name);
+    console.log(data.main.temp);
+    console.log(data.weather[0].description);
+});
+
+//create event listener for button
+document.getElementById("btn-forecast").addEventListener("click", getForecast);
+
+const results = document.getElementById("results");
+
+function getForecast(){
+    console.log("click");
+    //get zipcode
+    const zipcode = document.getElementById("zipcode").value ;
+    console.log("zipcode is " + zipcode);
+    //create url with this zipcode
+    const endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${apiKey}`;
+
+    //make an api request to get forecast for zipcode
+    fetch(endpoint)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+        //build html that goes in div
+        let forecast = `<h3>City: ${data.name} </h3>`;
+        results.innerHTML = forecast;
+        //display the temp in p tag
+        results.innerHTML += `<p>Temp: ${data.main.temp}</p>`
+        results.innerHTML += `<p>Description: ${data.weather[0].description}</p>`
+    })
+
+    //display the results on webpage
+} */
+
+
+
