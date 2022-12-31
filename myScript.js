1be48df8638660eb95f3c0c3239c53eaea3fbac2
mcfrems <matthew.fremstad@gmail.com>
2022-12-30 20:47:37 -0600

    javascript revisions

--- myScript.js
+++ myScript.js
@@ -1,8 +1,8 @@
 console.log(1+Math.floor(Math.random()*100))
 
-alert("hi");
+//alert("hi");
 
-const testurl =  "https://api.quotable.io/quotes"; //need to add pages to url I think
+const testurl =  "https://api.quotable.io/quotes?page=5"; //need to randomize page number
 
 //test the api
 fetch(testurl)
@@ -13,81 +13,32 @@ fetch(testurl)
 });
 
 //create event listener for button
-document.getElementById("new-quote").addEventListener("click", getQuote);
+document.getElementById("btn-new-quote").addEventListener("click", getQuote);
 
-function getQuote(){}
 
-/*const results = document.getElementById("results");
+function getQuote(){
+    //get new quote
+    let page = 1+Math.floor(Math.random()*100);
+    let quoteText = document.getElementById("quote-text");
+    let quoteAuthor = document.getElementById("quote-author");
+    //const quoteUp = document.getElementById("quote-up");
+    //const quoteDown = document.getElementById("quote-down");
+    console.log("quote text is " + quoteText);
+    console.log("quote author is" + quoteAuthor);
+    //console.log(quoteUp);
 
-function getForecast(){
-    console.log("click");
-    //get zipcode
-    const zipcode = document.getElementById("zipcode").value ;
-    console.log("zipcode is " + zipcode);
-    //create url with this zipcode
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${apiKey}`;
+    //create url with pagenumber
+    const endpoint = `https://api.quotable.io/quotes?page=${page}`
 
-    //make an api request to get forecast for zipcode
+    //make an api request to get new quote
     fetch(endpoint)
-    .then(res => res.json())
-    .then(data =>{
-        console.log(data);
-        //build html that goes in div
-        let forecast = `<h3>City: ${data.name} </h3>`;
-        results.innerHTML = forecast;
-        //display the temp in p tag
-        results.innerHTML += `<p>Temp: ${data.main.temp}</p>`
-        results.innerHTML += `<p>Description: ${data.weather[0].description}</p>`
+    .then(response => response.json())
+    .then(data => {
+  
+        let quote = data.results[0].content;
+        let author = data.results[0].author;
+  
+        quoteText.textContent = quote;
+        quoteAuthor.textContent = author;
     })
-
-
-
-
-
-
-    
-/*const apiKey = "acc785dd4ef0fe813c807aa2818b9994";
-
-const testurl = "https://api.openweathermap.org/data/2.5/weather?zip=68803&units=imperial&appid=acc785dd4ef0fe813c807aa2818b9994";
-
-//test the api
-fetch(testurl)
-.then(res =>res.json())
-.then(data => {
-    console.log(data);
-    console.log(data.name);
-    console.log(data.main.temp);
-    console.log(data.weather[0].description);
-});
-
-//create event listener for button
-document.getElementById("btn-forecast").addEventListener("click", getForecast);
-
-const results = document.getElementById("results");
-
-function getForecast(){
-    console.log("click");
-    //get zipcode
-    const zipcode = document.getElementById("zipcode").value ;
-    console.log("zipcode is " + zipcode);
-    //create url with this zipcode
-    const endpoint = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&units=imperial&appid=${apiKey}`;
-
-    //make an api request to get forecast for zipcode
-    fetch(endpoint)
-    .then(res => res.json())
-    .then(data =>{
-        console.log(data);
-        //build html that goes in div
-        let forecast = `<h3>City: ${data.name} </h3>`;
-        results.innerHTML = forecast;
-        //display the temp in p tag
-        results.innerHTML += `<p>Temp: ${data.main.temp}</p>`
-        results.innerHTML += `<p>Description: ${data.weather[0].description}</p>`
-    })
-
-    //display the results on webpage
-} */
-
-
-
+}
